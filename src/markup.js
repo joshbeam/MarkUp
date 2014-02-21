217d3e9c3cfbc27068b8b5c285ba6f1bda4eb5f7
joshbeam <joshua.a.beam@gmail.com>
2014-02-20 19:50:19 -0500

    fixed match type bug
    Different engines see absence of a match differently (either as an
    empty string, undefined, or either one).  Because “” and undefined both
    type coerce to false, existence can be checked without explicitly
    asking if(param!==undefined||param!==“”) //do something.  Instead, you
    can ask if(param), or if(!!param).

--- src/markup.js
+++ src/markup.js
@@ -11,24 +11,32 @@ Released under MIT license (http://opensource.org/licenses/MIT)
 										
 */
 
-function markup(element) {
+function markup(element,replaceTabs) {
 	
 	var elements = [].slice.call(element), i, j, numberOfElements = elements.length, tokens = [];
 				
 	function push(m,string,quote,comment,number,operator,key,bool,proto,regex) {
 		var type = "";
 		
-		if(string!==undefined||bool!==undefined||regex!==undefined) type = 'string';
-		if(comment!==undefined) type = 'comment';
-		if(number!==undefined) type = 'number';
-		if(operator!==undefined) type = 'operator';
-		if(key!==undefined) type = 'key';
-		if(proto!==undefined) type = 'proto';
+//		FireFox sees no match as an empty string.
+//		Safari sees no match as undefined or an empty string.
+//		Chrome sees no match as undefined.
+//		So --
+//		Since browsers coerce type when using if(), you could check 'truthiness' by if(comment), etc.
+//		In that case above, the engine will coerce 'comment' to evaluate to true if it is not "",undefined,0, etc.
+//		However, browsers also coerce 0,"",undefined to false if the not (!) operator is used
+//		Add two not operators (!!), and you get true if it exists and false if it doesn't (is this more 'stable'?)
+		if(!!string||!!bool||!!regex) type = 'string';
+		if(!!comment) type = 'comment';
+		if(!!number) type = 'number';
+		if(!!operator) type = 'operator';
+		if(!!key) type = 'key';
+		if(!!proto) type = 'proto';
 		
-		//Tokens are temporarily replaced with <!1>
-		//This resembles a tag, and this type of tag would never be used in a pre element
-		//Even if it was, and the user wanted to display it, it would have to be written at &lt;!1&gt;
-		//This is a better alternative than some pseudo-random token, like {!token-12937:match1}, etc.
+//		Tokens are temporarily replaced with <!i> (where i is any number starting from 0)
+//		This resembles a tag, and this type of tag would never be used in a pre element
+//		Even if it was, and the user wanted to display it, it would have to be written at &lt;!1&gt;
+//		This is a better alternative than some pseudo-random token, like {!token-12937:match1}, etc.
 		return '<!'+tokens.push('<span class="markup-'+type+'">'+m+'</span>')+'>';	
 	}
 	
@@ -38,16 +46,16 @@ function markup(element) {
 	
 	var regexes = [
 		
-		new RegExp('((["\']{1})[^"\'\\r\\n]*\\2)|' +    //	string
-				   '(\\/\\/[^\\n\\r]*|\\/\\*+[^(?:\\*\\/)]*\\*+\\/)|' +              //	comment
-				   '(\\d+)|' +                          //	number
-				   '(&lt;=?|&gt;=?|===?)|' +            //	operator
-				   '([\\w-_]+)(?=:)|' +                    //	key
-				   '(true|false)|' +                    //	bool
-				   '([A-Z]\\w*)|' +                       //	proto
+		new RegExp('((["\']{1})[^"\'\\r\\n]*\\2)|' + //string
+				   // BUG: multi-line comment fails if parens are inside the comment
+				   '(\\/\\/[^\\n\\r]*[\n\r]|\\/\\*+[^(?:\\*\\/)]*\\*+\\/)|' + //comment
+				   '(\\d+)|' + //number
+				   '(&lt;=?|&gt;=?|===?)|' + //operator
+				   '([\\w-_]+)(?=:)|' + //key
+				   '(true|false)|' + //bool
+				   '([A-Z]\\w+)|' + //proto
 				   '(\\/[^\\*][^\\/]*\\/[gmi]*(?![^gmi]))',
 				   'g'),
-//		/((["']{1})[^"'\r\n]*\2)|(\/\/[^\n\r]*)|(\d+)|(&lt;=?|&gt;=?|===?)/g,
 		push,
 		
 		//keyword
@@ -58,7 +66,7 @@ function markup(element) {
 				   ')\\b','gi'),
 		'<span class="markup-keyword">$1</span>',
 
-		//replace token marks, like {!1}, with their respective <span>match</span> from the tokens array
+//		replace token marks, like <!1>, with their respective <span>match</span> from the tokens array by index
 		/<!(\d+)>/g,
 		pop
 	], len = regexes.length;
@@ -68,6 +76,11 @@ function markup(element) {
 		
 		for(j=0;j<len;j+=2) {
 			html = html.replace(regexes[j],regexes[j+1]);
+		}
+		
+//		By default, replace tabs with 4 spaces
+		if(replaceTabs!==false) {
+			html = html.replace(/\t/g,'    ');	
 		}
 		
 		el.innerHTML = html;
